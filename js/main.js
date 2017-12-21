801e89138f0ae8b6655c0f1c36d0f74c7c9a771b
Speaker4TheUndead <jchance.is2cool@gmail.com>
2017-12-21 15:22:06 -0500

    Foldered everything and added bootstrap

---
+++ js/main.js
@@ -0,0 +1,4 @@
+$(document).ready(function () {
+    $(".base").append("<p>Big Data AnalysEnter the text you wish to analyze.is</p><textarea rows=\"10\" cols=\"50\">Enter the text you wish to analyze.</textarea>");
+    $("body").append("<button type=\"button\" class=\"btn btn-primary\">Primary</button>");
+});
