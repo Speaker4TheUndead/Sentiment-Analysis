f82f527aa14e26d2ab90d6d710957fdc20db4003
Speaker4TheUndead <jchance.is2cool@gmail.com>
2017-12-23 15:25:47 -0500

    Made the analyze function, and route.

--- server/server.py
+++ server/server.py
@@ -1,6 +1,24 @@
 from flask import Flask
 from flask import render_template
 from flask import request
+from textblob import TextBlob
+
+
+def analyze(text):
+    total = 0
+    avg = 0
+    blob = TextBlob(text)
+    for sentence in blob.sentences:
+        total += 1
+        # print(sentence.sentiment.polarity)
+        avg += sentence.sentiment.polarity
+
+    return str(avg / total)
+    # 0.060
+    # -0.341
+
+
+
 app = Flask(__name__)
 
 
@@ -8,12 +26,15 @@ app = Flask(__name__)
 def index(name=None):
     return render_template('index.html', name=name)
 
-@app.route('/ajaxtest', methods=['POST', 'GET'])
+@app.route('/analyze', methods=['POST'])
 def ajaxtest():
     error = None
-    if request.method == 'POST':
-        return str.capitalize(request.form['text'])
-    return "Eat my asshole"
+    if len(request.form['text']) > 5000:
+        error = "Too many Characters. Bad Request."
+    elif request.method == 'POST':
+        return analyze(request.form['text'])
+    else:
+        return "Eat my asshole"
 
 if __name__ == '__main__':
     app.run()
