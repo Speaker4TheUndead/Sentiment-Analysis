aa7b276117e3c15783dc2ff804f2544b2dd1d2a5
Sriganesh Gopal <sriganesh.iswar@gmail.com>
2017-12-26 22:03:47 -0500

    spacey mah boi, centered box and buttons

--- server/static/js/main.js
+++ server/static/js/main.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
     $("#base").append("<textarea id='text' rows=\"10\" cols=\"50\">Enter the text you wish to analyze.</textarea>");
     $("#buttonDiv").append("<input class='btn btn-info btn-large' id='naive_bayes' type='submit' value='Naive Bayes'>");
+    $('#buttonDiv').append('<p>     </p>')
     $("#buttonDiv").append("<input class='btn btn-info btn-large' id='pattern_matching' type='submit' value='Pattern Matching'>");
 
     $('#naive_bayes').click(function () {
