241a252931bacd76cd9cd747cccd91cb247be2d4
Sriganesh Gopal <sriganesh.iswar@gmail.com>
2017-12-26 21:43:31 -0500

    Added Naive Bayes and Pattern Matching selectors, centered box and buttons.

--- server/static/js/main.js
+++ server/static/js/main.js
@@ -1,7 +1,24 @@
 $(document).ready(function () {
     $("#base").append("<textarea id='text' rows=\"10\" cols=\"50\">Enter the text you wish to analyze.</textarea>");
-    $("#buttonDiv").append("<input class='btn btn-info btn-large' data-toggle='modal' datatarget='#myModal' type='submit'>");
-    $('#buttonDiv').click(function (){
+    $("#buttonDiv").append("<input class='btn btn-info btn-large' id='naive_bayes' type='submit' value='Naive Bayes'>");
+    $("#buttonDiv").append("<input class='btn btn-info btn-large' id='pattern_matching' type='submit' value='Pattern Matching'>");
+
+    $('#naive_bayes').click(function () {
+        var userText = $('#text').val();
+        $.ajax({
+            url: '/analyze_nv',
+            type: 'POST',
+            data: {
+                text: $('#text').val(),
+            },
+            dataType: 'text',
+            success: function (data) {
+                alert(data);
+            }
+        });
+    })
+
+    $('#pattern_matching').click(function () {
         var userText = $('#text').val();
         $.ajax({
             url: '/analyze',
@@ -10,10 +27,8 @@ $(document).ready(function () {
                 text: $('#text').val(),
             },
             dataType: 'text',
-            success: function(data){
+            success: function (data) {
                 alert(data);
-                $('#results').display.innerHTML = "";
-                $('#results').display.innerHTML += "<div id='myModal' class='modal fade' role='dialog'><div class='modal-dialog'><div class='modal-content'><div class='modal-header'><button type='button' class='close' data-dismiss='modal'>&times;</button><h4 class='modal-title'>Modal Header</h4></div><div class='modal-body'><p>Some text in the modal.</p></div><div class='modal-footer'><button type='button' class='btn btn-default' data-dismiss='modal'>Close</button></div></div></div></div>"
             }
         });
     })
